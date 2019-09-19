fab9e15793844f519491099ea3afa3d970e46721
Tobias Haase <tobhaase@gmail.com>
2019-09-19 12:28:47 +0200

    Small improvements

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 import requests
+from typing import Optional
 
 OWNER = "home-assistant"
 REPO = "home-assistant"
+URL = f"https://api.github.com/repos/{OWNER}/{REPO}/releases"
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def get_latest_version(self):
+    def get_latest_version(self) -> Optional[dict]:
+        """Return the latest release that's not a prerelease."""
+
+        # Request the github api for all releases
         try:
-            r = requests.get(
-                "https://api.github.com/repos/{}/{}/releases".format(OWNER, REPO)
-            )
+            r = requests.get(URL)
             r.raise_for_status()
         except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError):
             return None
 
+        # Find the latest release thats not a prerelease
         releases = json.loads(r.content)
         for release in releases:
             if release["prerelease"] is True:
@@ -27,6 +31,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             return result
 
     def do_GET(self):
+        """Handle a get request."""
         latest_version = self.get_latest_version()
         if latest_version is None:
             self.send_error(500, "The requested repository could not be reached.")
@@ -38,6 +43,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             return
 
     def do_POST(self):
+        """Handle a post request."""
         content_length = int(self.headers["Content-Length"])
         body = self.rfile.read(content_length)
         # do sth with that body
